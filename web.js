8cd5e6353e083a47d1770c38f74c493294bf40d1
VascoP <vascogpinho@gmail.com>
2014-04-10 20:22:04 +0100

    woop woop

--- web.js
+++ web.js
@@ -1,17 +1,33 @@
 var Hapi = require('hapi');
 
 var confirm = function (request, reply) {
-    console.log(reply);
+    console.log(request);
 };
 
 var cancel = function (request, reply) {
-    console.log(reply);
+    console.log(request);
 };
 
+var index = function (request, reply) {
+    reply.view('index.html', {
+        
+    });
+}
+
+var options = {
+    views: {
+        path: 'templates',
+        engines: {
+            html: 'handlebars'
+        },
+        partialsPath: 'partials'
+    }
+}; 
 
 var server = new Hapi.Server('localhost', 8000, { cors: true });
 
 server.route([
+    { method: 'GET', path: '/', handler: index },
     { method: 'GET', path: '/confirm', handler: confirm },
     { method: 'GET', path: '/cancel', handler: cancel }
 ]);
