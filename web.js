7aa4defa4cb6feac7da4eea7c5fbd373363eeac9
VascoP <vascogpinho@gmail.com>
2014-04-11 04:05:16 +0100

    new product + product detail

--- web.js
+++ web.js
@@ -1,5 +1,17 @@
 var Hapi = require('hapi');
+var Joi = require('joi');
 var wallet = require('meo-wallet');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+  if(!err) {
+    console.log("mongo up!");
+  }
+
+  product_table = db.collection('products');
+});
 
 var options = {
     views: {
@@ -9,20 +21,45 @@ var options = {
         },
         partialsPath: __dirname + '/partials'
     }
-}; 
+};
 
-var server = new Hapi.Server('localhost', 8000, options);
-
-server.route([
-    { method: 'GET', path: '/', handler: index },
-    { method: 'GET', path: '/go', handler: go },
-    { method: 'GET', path: '/confirm', handler: confirm },
-    { method: 'GET', path: '/cancel', handler: cancel },
-    { method: 'GET', path: '/{path*}', handler: {
-        directory: { path: './public', listing: true, index: true }
+var new_product_submit_config = {
+    handler: new_product_submit,
+    validate: {
+        payload: {
+            title: Joi.string().required(),
+            description: Joi.string().required(),
+            price: Joi.number().required().min(1).max(999),
+            email: Joi.string().email().required()
     } }
-]);
+};
 
+function new_product_submit(request, reply) {
+    console.log(request.payload);
+
+    var new_product = {
+        'title': request.payload.title,
+        'description': request.payload.description,
+        'price': request.payload.price,
+        'email': request.payload.email,
+    };
+
+    product_table.insert(new_product, function (err, item) {
+        var id = item[0]._id;
+
+        reply.view('product_detail.html', {
+            id: id,
+            title: request.payload.title,
+            description: request.payload.description,
+            price: request.payload.price,
+            email: request.payload.email
+        });
+    });
+};
+
+function new_product(request, reply) {
+    reply.view('new_product.html', {});
+};
 
 function confirm(request, reply) {
     reply.view('confirm.html', {});
@@ -34,6 +71,21 @@ function cancel(request, reply) {
 
 function index(request, reply) {
     reply.view('index.html', {});
+};
+
+function product_detail(request, reply) {
+    product_table.findOne({_id:ObjectId(request.params.id)}, function(err, item) {
+        console.log("ID: " + request.params.id);
+        if (err) {
+            console.log("ERR: " + err);
+        }
+        reply.view('product_detail.html', {
+            id: item._id,
+            title: item.title,
+            description: item.description,
+            price: item.price,
+        });
+    });
 };
 
 function go(request, reply) {
@@ -55,6 +107,21 @@ function go(request, reply) {
   });
 };
 
+
+var server = new Hapi.Server('localhost', 8000, options);
+
+server.route([
+    { method: 'GET', path: '/', handler: index },
+    { method: 'GET', path: '/go', handler: go },
+    { method: 'GET', path: '/confirm', handler: confirm },
+    { method: 'GET', path: '/cancel', handler: cancel },
+    { method: 'GET', path: '/new_product', handler: new_product },
+    { method: 'POST', path: '/new_product', config: new_product_submit_config },
+    { method: 'GET', path: '/products/{id}', handler: product_detail },
+    { method: 'GET', path: '/{path*}', handler: {
+        directory: { path: './public', listing: true, index: true }
+    } }
+]);
 
 server.start(function () {
     console.log('Server started at: ' + server.info.uri);
